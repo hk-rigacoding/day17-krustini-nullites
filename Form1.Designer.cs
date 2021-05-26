1df71ef54d13a8d47420947f602acc48f024490f
hk-rigacoding <hk.rigacoding@gmail.com>
2021-05-26 21:38:55 +0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿
+namespace day17_krustini_nullites
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.desasToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.sāktToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.bilde = new System.Windows.Forms.PictureBox();
+            this.status = new System.Windows.Forms.StatusStrip();
+            this.gaajiens = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.bilde)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.desasToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1070, 30);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // desasToolStripMenuItem
+            // 
+            this.desasToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.sāktToolStripMenuItem});
+            this.desasToolStripMenuItem.Name = "desasToolStripMenuItem";
+            this.desasToolStripMenuItem.Size = new System.Drawing.Size(62, 26);
+            this.desasToolStripMenuItem.Text = "Desas";
+            // 
+            // sāktToolStripMenuItem
+            // 
+            this.sāktToolStripMenuItem.Name = "sāktToolStripMenuItem";
+            this.sāktToolStripMenuItem.Size = new System.Drawing.Size(128, 26);
+            this.sāktToolStripMenuItem.Text = "Sākt !";
+            this.sāktToolStripMenuItem.Click += new System.EventHandler(this.sāktToolStripMenuItem_Click);
+            // 
+            // bilde
+            // 
+            this.bilde.Image = global::day17_krustini_nullites.Properties.Resources.Tic_tac_toe;
+            this.bilde.Location = new System.Drawing.Point(245, 44);
+            this.bilde.Name = "bilde";
+            this.bilde.Size = new System.Drawing.Size(604, 609);
+            this.bilde.TabIndex = 1;
+            this.bilde.TabStop = false;
+            // 
+            // status
+            // 
+            this.status.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.status.Location = new System.Drawing.Point(0, 743);
+            this.status.Name = "status";
+            this.status.Size = new System.Drawing.Size(1070, 22);
+            this.status.TabIndex = 2;
+            this.status.Text = "Gaajiens :";
+            // 
+            // gaajiens
+            // 
+            this.gaajiens.AutoSize = true;
+            this.gaajiens.Location = new System.Drawing.Point(146, 9);
+            this.gaajiens.Name = "gaajiens";
+            this.gaajiens.Size = new System.Drawing.Size(72, 17);
+            this.gaajiens.TabIndex = 3;
+            this.gaajiens.Text = "Gaajiens :";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.ClientSize = new System.Drawing.Size(1070, 765);
+            this.Controls.Add(this.gaajiens);
+            this.Controls.Add(this.status);
+            this.Controls.Add(this.bilde);
+            this.Controls.Add(this.menuStrip1);
+            this.DoubleBuffered = true;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Tic Tac Toe";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.bilde)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem desasToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem sāktToolStripMenuItem;
+        private System.Windows.Forms.PictureBox bilde;
+        private System.Windows.Forms.StatusStrip status;
+        private System.Windows.Forms.Label gaajiens;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,265 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace day17_krustini_nullites
+{
+    public partial class Form1 : Form
+    {
+        desu_laukums speele;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void sāktToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //instancēt desu_laukumu
+            speele = new desu_laukums();
+
+            for (int i = 0; i < 3; i++)
+                for (int j = 0; j < 3; j++)
+                {
+                    speele.set_desa(i, j, 120, 120, i*250 + 100, j*200 + 60);
+                    this.Controls.Add(speele.getDesa(i,j));
+                    speele.getDesa(i, j).BringToFront();
+                }
+
+
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            //laukums.Location = new Point(0,30);
+            //laukums.Size = new Size(this.ClientRectangle.Width, this.ClientRectangle.Height-30);
+            //laukums.SizeMode = PictureBoxSizeMode.StretchImage;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            bilde.Location = new Point(0, 30);
+            bilde.Size = new Size(this.ClientRectangle.Width, this.ClientRectangle.Height-30);
+            bilde.SizeMode = PictureBoxSizeMode.StretchImage;
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (speele == null)
+                return;
+
+            if (speele.X_gaajiens)
+                gaajiens.Text = "Gaajiens : X";
+            else
+                gaajiens.Text = "Gaajiens : O";
+
+
+
+            if (speele.uzvara > -1)
+            {
+                gaajiens.Text = "UZVARA !";
+                /*
+                //3x horizontālas rindas
+                uzvaras_varianti[0] = statuss[0, 0] + statuss[1, 0] + statuss[2, 0];
+                uzvaras_varianti[1] = statuss[0, 1] + statuss[1, 1] + statuss[2, 1];
+                uzvaras_varianti[2] = statuss[0, 2] + statuss[1, 2] + statuss[2, 2];
+
+                //3x vertikāles
+                uzvaras_varianti[3] = statuss[0, 0] + statuss[0, 1] + statuss[0, 2];
+                uzvaras_varianti[4] = statuss[1, 0] + statuss[1, 1] + statuss[1, 2];
+                uzvaras_varianti[5] = statuss[2, 0] + statuss[2, 1] + statuss[2, 2];
+
+                //2x diagonāles
+                uzvaras_varianti[6] = statuss[0, 0] + statuss[1, 1] + statuss[2, 2];
+                uzvaras_varianti[7] = statuss[2, 0] + statuss[1, 1] + statuss[0, 2];
+                */
+                switch (speele.uzvara)
+                {
+                    case 0:
+                        speele.getDesa(0, 0).BackColor = Color.Green;
+                        speele.getDesa(1, 0).BackColor = Color.Green;
+                        speele.getDesa(2, 0).BackColor = Color.Green;
+                        break;
+
+                    case 1:
+                        speele.getDesa(0, 1).BackColor = Color.Green;
+                        speele.getDesa(1, 1).BackColor = Color.Green;
+                        speele.getDesa(2, 1).BackColor = Color.Green;
+                        break;
+
+                    case 2:
+                        speele.getDesa(0, 2).BackColor = Color.Green;
+                        speele.getDesa(1, 2).BackColor = Color.Green;
+                        speele.getDesa(2, 2).BackColor = Color.Green;
+                        break;
+                    case 3:
+                        speele.getDesa(0, 0).BackColor = Color.Green;
+                        speele.getDesa(0, 1).BackColor = Color.Green;
+                        speele.getDesa(0, 2).BackColor = Color.Green;
+                        break;
+                    case 4:
+                        speele.getDesa(1, 0).BackColor = Color.Green;
+                        speele.getDesa(1, 1).BackColor = Color.Green;
+                        speele.getDesa(1, 2).BackColor = Color.Green;
+                        break;
+                    case 5:
+                        speele.getDesa(2, 0).BackColor = Color.Green;
+                        speele.getDesa(2, 1).BackColor = Color.Green;
+                        speele.getDesa(2, 2).BackColor = Color.Green;
+                        break;
+                    case 6:
+                        speele.getDesa(0, 0).BackColor = Color.Green;
+                        speele.getDesa(1, 1).BackColor = Color.Green;
+                        speele.getDesa(2, 2).BackColor = Color.Green;
+                        break;
+
+                    case 7:
+                        speele.getDesa(2, 0).BackColor = Color.Green;
+                        speele.getDesa(1, 1).BackColor = Color.Green;
+                        speele.getDesa(0, 2).BackColor = Color.Green;
+                        break;
+
+                }
+
+
+            }
+
+
+
+
+        }
+    }
+
+    // ko mums vajag krustiņiem - nullītēm ?
+
+    //spēles noteikumi ?
+    //trīs uzvar, rindā, kolonnā vai diagonāle
+    //liek viens pēc otra
+    //nepāra gājieni ir X un pāra gājieni ir O ?
+    //(sākam ar X)
+
+    // 9 pictureboksi
+    // bildes - X un O
+
+    class desu_laukums : PictureBox
+    {
+        //vai iet X ?
+       public bool X_gaajiens;
+        PictureBox[,] desas;
+        string[,] statuss;
+        public int uzvara;
+
+        // 9 pictureboksi
+        // bildes - X un O
+        public desu_laukums()
+        {
+            desas = new PictureBox[3, 3];
+            statuss = new string[3, 3];
+
+            for (int i = 0; i < 3; i++)
+                for (int j = 0; j < 3; j++)
+                    statuss[i, j] = "";
+
+                    X_gaajiens = true;
+            uzvara = -1;
+        }
+
+        int Calc()
+        {
+
+            //8 varianti
+            string[] uzvaras_varianti = new string[8];
+
+            //uzvaras_varianti[0] = "XXX";
+            //uzvaras_varianti[1] = "OOO";
+
+            //3x horizontālas rindas
+            uzvaras_varianti[0] = statuss[0, 0] + statuss[1, 0] + statuss[2, 0];
+            uzvaras_varianti[1] = statuss[0, 1] + statuss[1, 1] + statuss[2, 1];
+            uzvaras_varianti[2] = statuss[0, 2] + statuss[1, 2] + statuss[2, 2];
+
+            //3x vertikāles
+            uzvaras_varianti[3] = statuss[0, 0] + statuss[0, 1] + statuss[0, 2];
+            uzvaras_varianti[4] = statuss[1, 0] + statuss[1, 1] + statuss[1, 2];
+            uzvaras_varianti[5] = statuss[2, 0] + statuss[2, 1] + statuss[2, 2];
+
+            //2x diagonāles
+            uzvaras_varianti[6] = statuss[0, 0] + statuss[1, 1] + statuss[2, 2];
+            uzvaras_varianti[7] = statuss[2, 0] + statuss[1, 1] + statuss[0, 2];
+
+
+            for (int i=0; i< 8; i++)
+                if (uzvaras_varianti[i] == "XXX" || uzvaras_varianti[i] == "OOO")
+                {
+                    return i;
+                }
+
+            return -1;
+        }
+
+        public void set_desa(int i, int j, int sizeX, int sizeY, int locX, int locY)
+        {
+            desas[i, j] = new PictureBox();
+            desas[i, j].Size = new Size(sizeX, sizeY);
+            desas[i, j].Location = new Point(locX, locY);
+
+            //desas[i, j].Image = Properties.Resources.o;
+            //desas[i, j].SizeMode = PictureBoxSizeMode.StretchImage;
+
+            //mouse click, jo tas ļauj viegli noteikt labo/kreiso peles taustu !
+            desas[i, j].MouseClick += Desu_laukums_MouseClick;
+        }
+
+        public PictureBox getDesa(int i, int j)
+        {
+            return desas[i, j];
+        }
+
+
+        private void Desu_laukums_MouseClick(object sender, MouseEventArgs e)
+        {
+
+            //izpilda gājienu
+            for (int i=0; i< 3; i++)
+                for (int j = 0; j < 3; j++)
+                if (sender.Equals(desas[i, j]))
+                {
+
+                        if (statuss[i, j] != "")
+                            return;
+
+                        if (uzvara > -1)
+                            return;
+
+                        if (X_gaajiens)
+                        {
+                            desas[i, j].Image = Properties.Resources.x;
+                            statuss[i, j] = "X";
+                            desas[i, j].SizeMode = PictureBoxSizeMode.StretchImage;
+                            X_gaajiens = false;
+                        } else
+                        {
+                            desas[i, j].Image = Properties.Resources.o;
+                            statuss[i, j] = "O";
+                            desas[i, j].SizeMode = PictureBoxSizeMode.StretchImage;
+                            X_gaajiens = true;
+                        }
+
+                        break;
+                 }
+
+
+            //kalkulē rezultātu
+            uzvara = Calc();
+        }
+    }
+    
+}
